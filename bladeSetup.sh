a61810cd5027d0e5aa03f8054c042e010dd0d515
Matt VanCleave <matt.p.vancleave.com>
2016-06-04 10:16:32 -0700

    Adding Setup Shell Script

---
+++ bladeSetup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#sudo apt-get update && sudo apt-get upgrade -y
+
+# Git Hub Install and Config 
+
+#sudo apt-get install git -y 
+#git config --global user.email "matt.p.vancleave.com"
+#git config --global user.name "Matt VanCleave"
+#git init workspaceBladeVM
+#touch ~/workspaceBladeVM/README
+#echo "Workspace for Razer Blade Ubuntu 16.06 Virtual Machine" >> "~/workspaceVM/README" 
+#git add "~/workspaceVM/README" 
+#git commit -m "Setup"
+
